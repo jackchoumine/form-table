d470a2def202693c6be796390244c50a8ac532d9
jack <jackzhoumine@gmail.com>
2021-09-08 20:40:31 +0800

    chore: 配置对等依赖

--- prettier.config.js
+++ prettier.config.js
@@ -1,7 +1,8 @@
 module.exports = {
   semi: false,
-  trailingComma: "none",
+  trailingComma: 'none',
   singleQuote: true,
-  arrowParens: "avoid",
-  bracketSpacing: true
+  arrowParens: 'avoid',
+  bracketSpacing: true,
+  jsxSingleQuote: true // 在 jsx 中使用单引号
 }
